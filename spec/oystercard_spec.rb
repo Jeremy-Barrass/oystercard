00c27c45b4821ba9403d18feaf39d47679b49042
Jeremy Barrass <jeremybarrass@Jeremys-MacBook-Pro.local>
2016-01-25 16:54:39 +0000

    Balance spec and method added.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,13 @@
 require "oystercard"
 
 describe Oystercard do
+  subject(:oystercard) {described_class.new}
+  starting_balance = Oystercard::DEFAULT_BALANCE
 
+  it {is_expected.to respond_to(:balance)}
+  describe '#balance' do
+    it 'Has a default balance of 0' do
+      expect(oystercard.balance).to eq starting_balance
+    end
+  end
 end
