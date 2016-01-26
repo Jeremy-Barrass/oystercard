25045b7b07862a91dc7d0f9e78a22649920eba73
Jeremy Barrass <jeremybarrass@Jeremys-MacBook-Pro.local>
2016-01-26 10:01:58 +0000

    Challenge 4

---
+++ README.md
@@ -0,0 +1,5 @@
+This is an outline of the current test plan for this project.
+To begin, we will feature test for our Oystercard class, prepare a unit test and check for failure.
+Following this we will write our production code to pass the unit test, then test the feature in IRB.
+
+We will repeat this process for each new method or feature we wish to add.
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,16 +2,17 @@ require "oystercard"
 
 describe Oystercard do
   subject(:oystercard) {described_class.new}
+  start = Oystercard::DEFAULT_BALANCE
+  cash = 5
 
   context 'Money' do
     it {is_expected.to respond_to(:balance)}
     describe '#balance' do
       it 'Has a default balance of 0' do
-        expect(oystercard.balance).to eq Oystercard::DEFAULT_BALANCE
+        expect(oystercard.balance).to eq start
       end
 
       it 'Can have money added to it' do
-        cash = 5
         oystercard.add(cash)
         expect(oystercard.balance).to eq cash
       end
